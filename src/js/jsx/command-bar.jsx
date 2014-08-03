f6a88553219078a346f7e4ee73374fb240f87c24
fraserxu <xvfeng123@gmail.com>
2014-08-03 13:16:10 +0800

    make barchart vertical

--- src/js/jsx/command-bar.jsx
+++ src/js/jsx/command-bar.jsx
@@ -15,8 +15,8 @@ module.exports = React.createClass({
   render: function() {
     return (
       <rect fill={this.props.color}
-        width={this.props.width} height={this.props.height}
-        x={this.props.offset} y={this.props.availableHeight - this.props.height} />
+        height={this.props.height} width={this.props.width}
+        x={0} y={this.props.offset} />
     );
   }
 });
--- src/js/jsx/command-dataseries.jsx
+++ src/js/jsx/command-dataseries.jsx
@@ -17,17 +17,17 @@ module.exports = React.createClass({
   render: function() {
     var props = this.props;
 
-    var yScale = d3.scale.linear()
+    var xScale = d3.scale.linear()
       .domain([0, d3.max(this.props.data)])
       .range([0, this.props.height]);
 
-    var xScale = d3.scale.ordinal()
+    var yScale = d3.scale.ordinal()
       .domain(d3.range(this.props.data.length))
-      .rangeRoundBands([0, this.props.width], 0.05);
+      .rangeRoundBands([0, this.props.height], 0.05);
 
     var bars = _.map(this.props.data, function(point, i) {
       return (
-        <Bar height={yScale(point)} width={xScale.rangeBand()} offset={xScale(i)} availableHeight={props.height} color={props.color} key={i} />
+        <Bar width={xScale(point)} height={yScale.rangeBand()} offset={yScale(i)} availableWidth={props.width} color={props.color} key={i} />
       )
     });
 
